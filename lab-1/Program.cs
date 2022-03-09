778178fd89bc037f14712a95cb157fb52107dfc2
MeCatooo <79982959+MeCatooo@users.noreply.github.com>
2022-03-09 13:24:23 +0100

    koniec zajęć

--- lab-1/Program.cs
+++ lab-1/Program.cs
@@ -7,15 +7,35 @@ namespace lab_1
         static void Main(string[] args)
         {
             Person person = Person.Of("akj");
+            Person person1 = Person.Of("akj");
+            Console.WriteLine(person.Equals(person1));
             Money money = Money.OfWithException(1, Currency.PLN);
+            Money money1 = Money.OfWithException(2, Currency.PLN);
             Console.WriteLine(money.Value);
             Money result = money * 4;
             Console.WriteLine(result.Value);
             Money result1 = 2 * money;
             Console.WriteLine(result1.Value);
+            Console.WriteLine(money > money1);
+            Console.WriteLine((string)money);
+            Money[] pricies =
+            {
+                Money.Of(11,Currency.USD),
+                Money.Of(2,Currency.PLN),
+                Money.Of(13,Currency.EUR),
+                Money.Of(19,Currency.EUR),
+                Money.Of(1,Currency.USD),
+                Money.Of(16,Currency.PLN)
+            };
+            Array.Sort(pricies);
+            foreach(var p in pricies)
+            {
+                Console.WriteLine((string)p);
+            }
+
         }
     }
-    public class Person
+    public class Person:IEquatable<Person>
     {
         private string firstName;
         public string FirstName
@@ -52,6 +72,26 @@ namespace lab_1
                 throw new ArgumentOutOfRangeException("Imię zbyt krótkie");
             }
         }
+
+        public override string ToString()
+        {
+            return $"{firstName}";
+        }
+        public bool Equals(Person other)
+        {
+            return other.firstName.Equals(firstName);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&
+                   FirstName == person.FirstName;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(FirstName);
+        }
     }
     public enum Currency
     {
@@ -59,7 +99,7 @@ namespace lab_1
         USD = 2,
         EUR = 3
     }
-    public class Money
+    public class Money:IComparable<Money>
     {
         private readonly decimal _value;
         private readonly Currency _currency;
@@ -110,6 +150,58 @@ namespace lab_1
         public static Money operator *(decimal factor, Money money)
         {
             return Money.OfWithException(money.Value * factor, money.Currency);
+        }
+        public static Money operator +(Money money1, Money money2)
+        {
+            IsSameCurrencies(money1, money2);
+            return Money.OfWithException((money1.Value + money2.Value), money1.Currency);
+        }
+        public static bool operator >(Money a, Money b)
+        {
+            IsSameCurrencies(a, b);
+            return a.Value > b.Value;
+        }
+        public static bool operator <(Money a, Money b)
+        {
+            IsSameCurrencies(a, b);
+            return a.Value < b.Value;
+        }
+        private static void IsSameCurrencies(Money a, Money b)
+        {
+            if (a.Currency != b.Currency)
+            {
+                throw new ArgumentException("Diffrent currencies");
+            }
+        }
+        public static implicit operator decimal(Money money)
+        {
+            return money.Value;
+        }
+        public static explicit operator double(Money money)
+        {
+            return (double)money.Value;
+        }
+        public static explicit operator string(Money money)
+        {
+            return $"{money.Value} {money.Currency}";
+        }
+
+        public override string ToString()
+        {
+            return $"{_value} {_currency}";
+        }
+
+        public int CompareTo(Money other)
+        {
+            int currencyCon = Currency.CompareTo(other.Currency);
+            if(currencyCon == 0)
+            {
+                return Value.CompareTo(other.Value);
+            }
+            else
+            {
+                return currencyCon;
+            }
         }
     }
 
